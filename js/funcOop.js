a84e4499a62475fc2375f675ae7b8e65f8be85dc
AN <drgoncom8@gmail.com>
2025-01-07 22:20:34 +0200

    Update funcOop.js

--- js/funcOop.js
+++ js/funcOop.js
@@ -5,8 +5,9 @@ class GradesAndGPAcalc {
     this.Percentage = 0;
     this.GPA = 0;
     this.Grade = "F";
-    this.Counter = 0;
+    this.state = false;
   }
+
   GetGradsAndCredits() {
     const GradesAndCreditsArray = [];
     const ChildrenCount = document.getElementById(
@@ -17,78 +18,111 @@ class GradesAndGPAcalc {
     let Credits = document.querySelectorAll(`.credits`);
     for (let i = 0; i < ChildrenCount; i++) {
       if (
+        Grades[i].value != "" &&
         Grades[i].value >= 0 &&
         Grades[i].value <= 100 &&
+        Credits[i].value != "" &&
         Credits[i].value >= 0 &&
         Credits[i].value <= 10
       ) {
+        !this.state ? (this.state = true) : "";
         GradesAndCreditsArray.push([
           parseFloat(Grades[i].value),
           parseFloat(Credits[i].value),
         ]);
       } else {
         GradesAndCreditsArray.length = 0;
+        this.state = false;
         break;
       }
     }
     this.GradesAndCreditsList = GradesAndCreditsArray;
   }
-  computePercentage() {
+  computePercentageAndGPA() {
     if (this.GradesAndCreditsList.length > 0) {
       let totalPoints = 0;
+      let totalGPA = 0;
       let totalCredits = 0;
       for (const [grade, credit] of this.GradesAndCreditsList) {
         totalPoints += grade * credit;
         totalCredits += credit;
+        if (grade > 100 || grade < 0) {
+          totalGPA = "wrong";
+        } else if (grade >= 95) {
+          totalGPA += 4 * credit;
+        } else if (grade >= 90) {
+          totalGPA += 3.7 * credit;
+        } else if (grade >= 85) {
+          totalGPA += 3.4 * credit;
+        } else if (grade >= 80) {
+          totalGPA += 3 * credit;
+        } else if (grade >= 75) {
+          totalGPA += 2.7 * credit;
+        } else if (grade >= 70) {
+          totalGPA += 2.3 * credit;
+        } else if (grade >= 65) {
+          totalGPA += 2 * credit;
+        } else if (grade >= 60) {
+          totalGPA += 1.7 * credit;
+        } else if (grade >= 55) {
+          totalGPA += 1.3 * credit;
+        } else if (grade >= 50) {
+          totalGPA += 1 * credit;
+        }
       }
+      console.log();
 
       this.Percentage =
         totalCredits > 0 ? (totalPoints / totalCredits).toFixed(2) : 0;
+      this.GPA = totalCredits > 0 ? (totalGPA / totalCredits).toFixed(2) : 0;
     }
   }
+
   ComputeGradeAndGpa() {
-    if (this.Percentage > 100 || this.Percentage < 0) {
+    if (this.GPA > 4 || this.GPA < 0) {
       this.Grade = "wrong";
-    } else if (this.Percentage >= 95) {
+    } else if (this.GPA == 4) {
       this.Grade = "A+";
-    } else if (this.Percentage >= 90) {
+    } else if (this.GPA >= 3.7) {
       this.Grade = "A";
-    } else if (this.Percentage >= 85) {
+    } else if (this.GPA >= 3.4) {
       this.Grade = "A-";
-    } else if (this.Percentage >= 80) {
+    } else if (this.GPA >= 3) {
       this.Grade = "B+";
-    } else if (this.Percentage >= 75) {
+    } else if (this.GPA >= 2.7) {
       this.Grade = "B";
-    } else if (this.Percentage >= 70) {
+    } else if (this.GPA >= 2.3) {
       this.Grade = "C+";
-    } else if (this.Percentage >= 65) {
+    } else if (this.GPA >= 2) {
       this.Grade = "C";
-    } else if (this.Percentage >= 60) {
+    } else if (this.GPA >= 1.7) {
       this.Grade = "D+";
-    } else if (this.Percentage >= 55) {
+    } else if (this.GPA >= 1.3) {
       this.Grade = "D";
-    } else if (this.Percentage >= 50) {
+    } else if (this.GPA >= 1) {
       this.Grade = "D-";
     }
-    this.GPA = ((this.Percentage * 4) / 100).toFixed(2);
   }
 }
 class Ui {
   static createElement() {
     let ChildrenCount = GradesAndCreditsDiv.children.length;
     let element = document.createElement("div");
-    element.className = `flex`;
-    element.id = `sub${ChildrenCount + 1}`
+    element.className = `filed`;
+    element.id = `sub${ChildrenCount + 1}`;
     element.innerHTML = `
-        
-        <input class="three-width" type="text" name="SubName${
+        <div  class="">
+        <input class=" flex"  type="text" name="SubName${
           ChildrenCount + 1
         }" placeholder="Subject ${ChildrenCount + 1}" />
+        </div>
+        
+        <div  class="full-width">
         <input
-        class="three-width grades"
-                type="number"
-                name="Grade${ChildrenCount + 1}"
-                min="0"
+        class="two-width grades  flex"
+        type="number"
+        name="Grade${ChildrenCount + 1}"
+        min="0"
                 max="100"
                 required
                 placeholder=" Grade ${ChildrenCount + 1}"
@@ -96,31 +130,32 @@ class Ui {
               />
 
               <input
-                class="three-width credits"
+                class="two-width credits  "
                 type="number"
                 name="credit${ChildrenCount + 1}"
                 min="0"
                 max="10"
                 required
               
-                placeholder="Credit ${ChildrenCount + 1}"
-              />
-            
-            `;
+                placeholder="Credit ${ChildrenCount + 1}"/>
+                </div>
+                `;
     GradesAndCreditsDiv.appendChild(element);
   }
 
   static handleUi(calc) {
     function ShowResults() {
-      let result = document.getElementById("result");
-      result.innerHTML = `<div>Percentage: ${calc.Percentage}%</div>
-      <div>Grade: ${calc.Grade}</div>
-      <div>GPA: ${calc.GPA}</div>`;
+      if (calc.state) {
+        let result = document.getElementById("result");
+        result.innerHTML = `<div>Percentage: ${calc.Percentage}%</div>
+        <div>Grade: ${calc.Grade}</div>
+        <div>GPA: ${calc.GPA}</div>`;
+      }
     }
-    const calcBtn = document.querySelector(`.btn`);
+    const calcBtn = document.querySelector(`#calc`);
     calcBtn.onclick = function () {
       calc.GetGradsAndCredits();
-      calc.computePercentage();
+      calc.computePercentageAndGPA();
       calc.ComputeGradeAndGpa();
       ShowResults();
     };
@@ -135,14 +170,14 @@ class Ui {
     let AddBtn = document.getElementById("add");
     let DeleteBtn = document.getElementById("delete");
     AddBtn.onclick = function () {
-      if (GradesAndCreditsDiv.children.length <= 20) {
+      if (GradesAndCreditsDiv.children.length < 20) {
         Ui.createElement();
       }
     };
     DeleteBtn.onclick = function () {
       let ChildrenCount = GradesAndCreditsDiv.children.length;
-     let deletedElement = document.getElementById(`sub${ChildrenCount}`)
-     GradesAndCreditsDiv.children.length > 0 ? deletedElement.remove() : ""
+      let deletedElement = document.getElementById(`sub${ChildrenCount}`);
+      ChildrenCount > 0 ? deletedElement.remove() : "";
     };
   }
   static RestButton() {
