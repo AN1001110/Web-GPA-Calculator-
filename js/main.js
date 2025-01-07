4efbf90c8b409339c0a7603ef533260403b1fc1a
AN <drgoncom8@gmail.com>
2025-01-07 22:28:32 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// import {GetGradsAndCredits,computePercentage,ComputeGradeAndGpa,copyright} from "./functions.js"
-// const calcBtn = document.querySelector(`.btn`);
-// const disabled = document.querySelector(`form`);
-// disabled.onsubmit = function (element) {
-//   element.preventDefault();
-// };
-// calcBtn.onclick = function () {
-//   let gradesAndCredits = GetGradsAndCredits();
-//   let percentage = computePercentage(gradesAndCredits);
-//   let GPA, Grade;
-//   [GPA, Grade] = ComputeGradeAndGpa(percentage);
-//   console.log(gradesAndCredits);
-//   console.log(percentage);
-//   console.log(GPA);
-//   console.log(Grade);
-// };
-// copyright();
 import {Ui,GradesAndGPAcalc} from "./funcOop.js"
 const calculator = new GradesAndGPAcalc(`GradesAndCreditsDiv`)
 Ui.preventFormSubmission()
